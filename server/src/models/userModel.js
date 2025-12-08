1373ada0c825cba7fdb0962ead927033dcdf1204
sridip-de <sridipdey630@gmail.com>
2025-12-08 22:13:20 +0530

    Add user model with password hashing and JWT token generation methods; update environment variables

--- server/src/models/userModel.js
+++ server/src/models/userModel.js
@@ -1,4 +1,7 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import ApiError from '../utils/ApiError.js';
 
 const userSchema = new mongoose.Schema({
   name:{type: String, required: true},
@@ -6,8 +9,52 @@ const userSchema = new mongoose.Schema({
   password:{type: String, required: true},
   verifyOtp:{type: String, default:""},
   verifyOtpExpireAt:{type: Number, default:0},
-  isAccauntVerified:{type: Boolean, default:false},
+  isAccountVerified:{type: Boolean, default:false},
+  refreshToken: {type: String, default:""},
 })
+
+// Middlewares
+userSchema.pre('save', async function (next) {
+  if(!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+})
+
+// Methods
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+}
+
+userSchema.methods.getAccessToken = function () {
+  return jwt.sign(
+    { _id: this._id, email: this.email },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_LIFE }
+  )
+}
+
+userSchema.methods.getRefreshToken = function () {
+  return jwt.sign(
+    { _id: this._id,},
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_LIFE}
+  )
+}
+
+userSchema.methods.generateAccessAndRefreshToken = async function () {
+  try {
+    const accessToken = this.getAccessToken();
+    const refreshToken = this.getRefreshToken();
+  
+    this.refreshToken = refreshToken;
+    await this.save({validateBeforeSave: false});
+    
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(500,"Token Generation Failed");
+  }
+}
+
 
 const userModel = mongoose.models.user || mongoose.model("user", userSchema); 
 // to prevent model overwrite issue in watch mode
