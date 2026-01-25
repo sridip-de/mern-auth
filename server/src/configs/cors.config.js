a712fa29c5d90177b4e02b02107dea6109f9ffa5
sridip-de <sridipdey630@gmail.com>
2026-01-25 13:28:36 +0530

    updated the cors config for non-ajax requests

--- server/src/configs/cors.config.js
+++ server/src/configs/cors.config.js
@@ -1,17 +1,24 @@
-const whitelist = ['http://localhost:4173', 'http://localhost:5173']
+const whitelist = ['http://localhost:4173', 'http://localhost:5173', 'https://google.com']
 
 const CORS_CONFIG = {
   origin: function (origin, callback) {
+    console.log(origin)
+    if(!origin) {
+      return callback (null, true);
+    }
     if(whitelist.indexOf(origin) !== -1 ) { // see docs bellow 
       callback(null, true)
     } else {
       callback(new Error('Not allowed by CORS'))
     }
+
+    
   } ,
   credentials:true
 }
 
 export default CORS_CONFIG;
+
 
 // (origin, callback)
 // 1. origin --> the url from where request is coming
