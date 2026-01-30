302523ae6415e73e2711dc025f1a67a2e9b52efb
sridip-de <sridipdey630@gmail.com>
2026-01-31 00:29:32 +0530

    added common loader component

---
+++ client/src/components/common/Loader/Loader.jsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+  return (
+    <div className=" h-[calc(100vh-60px)] bg-zinc-800 text-zinc-200 text-4xl text-center items-center justify-center">
+      Loading...
+    </div>
+  )
+}
+
+export default Loader
