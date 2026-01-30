dee78d8f505c2f2844c7696cc1398ff36971466f
sridip-de <sridipdey630@gmail.com>
2026-01-31 01:40:37 +0530

    added zustand persist to survive hot reload

--- client/src/store/queryState.store.js
+++ client/src/store/queryState.store.js
@@ -1,14 +1,22 @@
 import { create } from "zustand";
+import { persist } from 'zustand/middleware';
 
-const stateStore = create((set) => ({
-  enabled: false,
-
-  setEnabled: (data) => set({ enabled: data })
-}))
+const stateStore = create(
+  persist(
+    (set) => ({
+      enabled: false,
+      setEnabled: (data) => set({ enabled: data })
+    }),
+    {
+      name: 'query-toggle-storage'
+    }
+  )
+)
 
 export const useQueryState = () => {
   return {
     queryState: stateStore(state => state.enabled),
-    setQueryState: stateStore(state => state.setEnabled)
+    setQueryState: stateStore(state => state.setEnabled),
+    clearQueryState: stateStore(state => state.clearStorage)
   }
 }
