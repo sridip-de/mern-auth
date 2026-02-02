4d85984de1570e1c43f7ecd74422b3de8e57a043
sridip-de <sridipdey630@gmail.com>
2026-02-03 00:40:39 +0530

    updated the cookies config for cross site

--- server/src/constants/cookieOptions.constants.js
+++ server/src/constants/cookieOptions.constants.js
@@ -2,13 +2,13 @@ const COOKIE_OPTIONS = {
   ACCESS_TOKEN_OPTIONS: {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
     maxAge: 15 * 60 * 1000, // 15 minutes
   },
   REFRESH_TOKEN_OPTIONS: {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days  
   },
 }
