1d47544ba15d4690bb71a7745d0405512d6d3ad2
sridip-de <sridipdey630@gmail.com>
2026-02-10 14:31:03 +0530

    updated the service url endpoint and the image property name to profile-picture

--- client/src/features/user/services/mediaService.js
+++ client/src/features/user/services/mediaService.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 import APP_ROUTES from "@/constants/app.routes";
+import axiosInstance from "@/config/axios.config";
 
 export const uploadImage = async ({ file, onProgress }) => {
   const formData = new FormData();
-  formData.append('file', file);
+  formData.append('profile-picture', file);
 
-  return await axios.post('https://api.escuelajs.co/api/v1/files/upload', formData, {
+  return await axiosInstance.post('/uploads/profile', formData, {
     onUploadProgress: (progressEvent) => {
       const percentCompleated = Math.round(
         (progressEvent.loaded * 100) / progressEvent.total);
